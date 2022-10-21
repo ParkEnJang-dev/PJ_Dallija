87fb3b70660aa16150bdb8bd6db73c4225c1b38b
min-gui <msj349911@gmail.com>
2022-10-21 14:26:50 +0900

    orderItem 에서 order JoinColum 이름 변경

--- src/main/java/com/spring/dallija/domain/order/OrderItem.java
+++ src/main/java/com/spring/dallija/domain/order/OrderItem.java
@@ -20,7 +20,7 @@ public class OrderItem {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_seq")
+    @JoinColumn(name = "item_id")
     private Item item;
 
     @ManyToOne(fetch = FetchType.LAZY)
