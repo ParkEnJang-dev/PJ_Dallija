19dd523bd69a9468f7db2cd4583ff4da7a6d2535
min-gui <msj349911@gmail.com>
2022-10-26 11:20:49 +0900

    userRepository 재정의

--- src/main/java/com/spring/dallija/repository/UserRepository.java
+++ src/main/java/com/spring/dallija/repository/UserRepository.java
@@ -7,9 +7,6 @@ import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-     User save(User user);
-     Optional<User> findById(Long id);
      Optional<User> findByEmail(String email);
 
 }
