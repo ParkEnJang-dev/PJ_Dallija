ff98475d4e9e07b6d71898cefb64f058c54e08fc
min-gui <msj349911@gmail.com>
2022-11-27 23:09:40 +0900

    uniTest 회원 장바구니 조회

--- src/test/java/com/spring/dallija/service/CartServiceTest.java
+++ src/test/java/com/spring/dallija/service/CartServiceTest.java
@@ -1,5 +1,6 @@
 package com.spring.dallija.service;
 
+import com.spring.dallija.controller.dto.CartDto;
 import com.spring.dallija.domain.cart.Cart;
 import com.spring.dallija.domain.item.Item;
 import com.spring.dallija.domain.user.User;
@@ -13,7 +14,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.spring.dallija.controller.dto.CartDto.SaveCartRequest;
@@ -59,6 +65,28 @@ class CartServiceTest {
         assertThat(resultCart.getQuantity()).isEqualTo(2);
      }
 
+    @Test
+    public void 회원_장바구니_조회() throws Exception {
+        //given
+        User user = new User(0L,"min","min@naver.com");
+        Item item = new Item("소고기 볶음",2000,10,"횡성");
+        SaveCartRequest saveCartRequest = new SaveCartRequest(4L,2);
+        Cart cart = Cart.createCart(user, item, saveCartRequest.getQuantity());
 
+        List<CartDto.CartResponse> list = List.of(CartDto.CartResponse.CreateCartResponse(cart));
+        Pageable pageable = PageRequest.of(0,10);
+        Page<CartDto.CartResponse> result = new PageImpl<>(list,pageable,list.size());
+
+        given(cartRepository.findUserCarts(0L,pageable)).willReturn(result);
+        when(userRepository.findByEmail("min@naver.com")).thenReturn(Optional.of(user));
+
+        //when
+        Page<CartDto.CartResponse> userCartItems = cartService.findUserCartItems("min@naver.com", pageable);
+
+        //then
+        assertThat(userCartItems.getTotalPages()).isEqualTo(1);
+        assertThat(userCartItems.getContent().get(0).getItemName()).isEqualTo("소고기 볶음");
+        assertThat(userCartItems.getContent().get(0).getItemQuantity()).isEqualTo(2);
+    }
 
 }
